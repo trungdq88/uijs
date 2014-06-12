a09d48a0b5bd37840271963b295e07d7a1c4cab6
Dinh Quang Trung <trungdq88@gmail.com>
2014-06-13 01:13:54 +0700

    Update framework

--- src/uijs/app/modules/backend_service/init.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by TrungDQ3 on 6/9/14.
- */
-$coreStartup.done(function () {
-    window.$backendService = new BackendService();
-});
--- src/uijs/app/modules/frame_manager/init.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by TrungDQ3 on 6/9/14.
- */
-$coreStartup.done(function () {
-    window.$frameManager = new FrameManager();
-});
